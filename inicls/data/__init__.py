298a3d1e34dea214f4ea14fcb0f13a89ce27ba3f
muyun99 <27944418+Muyun99@users.noreply.github.com>
2021-06-17 23:42:42 +0800

    [detele] delete trash code of cifar dataset

--- inicls/data/__init__.py
+++ inicls/data/__init__.py
@@ -1,4 +1,3 @@
 # from .implement import MyDataSet, transform_train, transform_valid, transform_test, label2int, int2label
-from .implement import build_trainval_dataset, build_test_dataset
 from .builder import build_dataset, build_dataloader
-__all__ = ['build_trainval_dataset', 'build_test_dataset', 'build_dataset', 'build_dataloader']
+__all__ = ['build_dataset', 'build_dataloader']
